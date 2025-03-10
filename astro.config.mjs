354be6aed17da89a30b718c4f66fddd4f1bec92f
Jasen Lo <jasenlo123@gmail.com>
2025-03-10 00:01:26 -0400

    maptiler key

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+// @ts-nocheck
+import { defineConfig, envField } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
-  }
+  },
 });
